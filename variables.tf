583f351fd3b2e4e065d5aab47db5c2717a23b6e0
Ganesh-Surya-Manimohan <surya.manimohan@gmail.com>
2025-03-19 12:53:23 +0530

    SG Module created

--- variables.tf
+++ variables.tf
@@ -4,14 +4,44 @@ variable "project_name" {
 
 variable "environment" {
     type = string
-    default = "dev"
 }
 
 variable "sg_name" {
     type = string
-    default =
 }
 
 variable "sg_description" {
     type = string
+}
+
+variable "vpc_id" {
+    type = string
+}
+
+variable "common_tags" {
+  type        = map
+}
+
+variable "sg_tags" {
+    type = map
+    default = {}
+}
+
+variable "outbound_rules" {
+    type = list
+    default = [
+        {
+            from_port = 0
+            to_port = 0
+            protocol = "-1" # all protocols
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    ]
+    
+}
+
+variable "inbound_rules" {
+    type = list
+    default = []
+    
 }
